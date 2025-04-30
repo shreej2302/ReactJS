9fec7459f63280db98eb033c03342993a64e8737
shreej2302 <163439838+shreej2302@users.noreply.github.com>
2025-04-30 19:14:32 +0530

    Simple App using React JS

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (task) => {
+    const newTodo = {id: Date.now(), task, completed: false};
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  return (
+    <div>
+      <h1>My To-Do App</h1>
+      <TodoInput addTodo={addTodo} />
+      <TodoList todos={todos}
+        toggleComplete={toggleComplete}
+        deleteTodo={deleteTodo}
+      />
+    </div>
+  );
+}
+
+export default App;
